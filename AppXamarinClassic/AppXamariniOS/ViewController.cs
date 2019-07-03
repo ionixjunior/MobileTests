fc11339b3094bfa24517429e7d0940a30022e3a9
Ione Souza Junior <junior@ionixjunior.com.br>
2019-07-03 12:29:48 -0300

    Adicionar exemplo do iOS com Xamarin.iOS

---
+++ AppXamarinClassic/AppXamariniOS/ViewController.cs
@@ -0,0 +1,21 @@
+﻿using Foundation;
+using System;
+using System.Net;
+using UIKit;
+
+namespace AppXamariniOS
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        async partial void onLoadDataClick(UIButton sender)
+        {
+            var client = new WebClient();
+            var data = await client.DownloadStringTaskAsync("https://mobiletests.getsandbox.com/ping");
+            lblData.Text = data;
+        }
+    }
+}
---
+++ AppXamarinClassic/AppXamariniOS/ViewController.designer.cs
@@ -0,0 +1,42 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace AppXamariniOS
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnLoadData { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblData { get; set; }
+
+        [Action ("onLoadDataClick:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void onLoadDataClick (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (btnLoadData != null) {
+                btnLoadData.Dispose ();
+                btnLoadData = null;
+            }
+
+            if (lblData != null) {
+                lblData.Dispose ();
+                lblData = null;
+            }
+        }
+    }
+}
