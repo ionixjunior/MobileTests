55b29d08277afe8b98a2ca403687cbb863ca9929
Ione Souza Junior <junior@ionixjunior.com.br>
2019-07-04 12:28:25 -0300

    Adicionar exemplo com Xamarin.Forms

---
+++ AppXamarinForms/AppXamarinForms/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace AppXamarinForms
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private readonly DataService _dataService;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _dataService = new DataService();
+        }
+
+        private async void OnLoadDataClicked(object sender, EventArgs e)
+        {
+            lblData.Text = await _dataService.LoadData();
+        }
+    }
+}
